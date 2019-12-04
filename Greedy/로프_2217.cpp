032effb23ae01085bce2ecdc066eb293dbfa93bf
mooss <antjdlekd123@naver.com>
2019-12-04 16:58:09 +0900

    백준 2217번 문제출처 : https://www.acmicpc.net/problem/2217

---
+++ Greedy/로프_2217.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<cstdio>
+#include<stack>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<ctime>
+#include<set>
+#include<sstream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans = max(ans, v[i] * (n - i));
+	}
+	cout << ans;
+}
